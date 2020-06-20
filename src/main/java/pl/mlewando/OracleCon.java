499535c4b6f62eb714cfb852a3dcbc89fd95a7cf
Miłosz Lewandowski <milosz.hg@gmail.com>
2020-06-20 18:19:49 +0200

    FEAT: (database) Add JDBC driver and setup db connection, fixes: #8

---
+++ src/main/java/pl/mlewando/OracleCon.java
@@ -0,0 +1,23 @@
+package pl.mlewando;
+
+import java.sql.*;
+class OracleCon{
+    public static void main(String args[]){
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection con=DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe","milosz","milosz");
+
+            Statement stmt=con.createStatement();
+
+            ResultSet rs=stmt.executeQuery("select * from TEST");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+
+            con.close();
+
+        }catch(Exception e){ System.out.println(e);}
+
+    }
+}
