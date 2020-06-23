72f9cdef14d36861664a09642fa87984117ddf70
Miłosz Lewandowski <milosz.hg@gmail.com>
2020-06-23 08:36:22 +0200

    FEAT(data access logic): Sql statement for business logic in ex.1, fixes #11

---
+++ sql/ex1query.sql
@@ -0,0 +1,15 @@
+--ZAD1:
+--Dział księgowości pewnej firmy oczekuje akcji wyświetlającej nazwiska pracowników,
+--którzy pracowali przynajmniej raz w departamencie o nazwie „DRK”.
+--Księgowy oczekuje, że informacje powinny być wyświetlone w konsoli w kolejności alfabetycznej wg nazwiska pracownika.
+
+--TODO: optymalizacja
+--agregowanie wszystkich tabel po parametrze department id, a więc często duplikatów.
+--zastanowić się jak omijać job history w których dany pracowni ktualnie pracuje w poszukiwanym departamencie
+
+SELECT employees.last_name FROM departments
+                                    join job_history ON job_history.department_id = departments.department_id
+                                    join employees ON employees.department_id = departments.department_id
+where departments.department_name = 'IT'
+GROUP By employees.last_name --chyba że pomijamy grupowanie po nazwisku w przypadku innych emp.id.
+ORDER BY employees.last_name ASC;
